48227a08ee2e9e099f90a930cc934379104044d3
naranrosa <natan.rodrigues10@gmail.com>
2025-09-04 12:37:35 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -82,7 +82,7 @@ const formatMonthYear = (date: Date) => {
 
 
 // --- CONSTANTS ---
-const expenseCategories = ['Alimentação', 'Moradia', 'Lazer', 'Saúde', 'Transporte', 'Cartão', 'igreja', 'Outros'];
+const expenseCategories = ['Alimentação', 'Moradia', 'Lazer', 'Médico', 'Transporte', 'Cartão', 'igreja', 'dividas', 'Farmácia', 'Outros'];
 const incomeCategories = ['Salário', 'Freelancer', 'Serviços', 'Outros'];
 const allCategories = [...new Set([...expenseCategories, ...incomeCategories])];
 
